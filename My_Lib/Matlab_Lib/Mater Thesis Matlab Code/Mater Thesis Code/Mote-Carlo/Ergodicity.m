46af487c4af0566d389c726a048647b8ebe25495
Penn <panbaoxiang@hotmail.com>
2015-11-03 16:46:21 -0800

    as

--- My_Lib/Matlab_Lib/Mater Thesis Matlab Code/Mater Thesis Code/Mote-Carlo/Ergodicity.m
+++ My_Lib/Matlab_Lib/Mater Thesis Matlab Code/Mater Thesis Code/Mote-Carlo/Ergodicity.m
@@ -1,4 +1,4 @@
-function m=Ergodicity()
+function [m,n]=Ergodicity()
 result=[];
 day_a=8;
 dep_a=0.4;
@@ -6,15 +6,31 @@ initials=0.3;
 initialep=3;
 b=0;
 m=[];
-entro=[];
+n=[];
 for i=1:1000
     [rain I ep r s]=Mont_Carlo(initials,initialep,day_a,dep_a,b);
-    result=[result s(1:150,:)];
+    result=[result s(1:400,:)];
+    tempmean=[];
+    for j=1:400
+        tempmean=[tempmean mean(s(1:j))];
+    end
+    n=[n tempmean'];
 end
 for i=1:size(result,1)
     z=mean(result(i,:));
     m=[m z];
 end 
-xlabel('Time/d');
-ylabel('Ensemble Average Soil Moisture');
+ 
+
+for i=1:400
+    n=[n mean(s(1:i))];
+end
+
+plot(m');
+xlabel('Time/d','FontSize',24); 
+ylabel('Average Soil Moisture','FontSize',24);
+hold on;
+plot(n');
+legend('Ensemble Average','Temporal Average');
+
 end
