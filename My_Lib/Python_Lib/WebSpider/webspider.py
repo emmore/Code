fa4ba4761359b79973e80d2a5cac046e3df4fab1
Penn <panbaoxiang@hotmail.com>
2015-11-08 22:48:26 -0800

    done!
    webspider v1

--- My_Lib/Python_Lib/WebSpider/webspider.py
+++ My_Lib/Python_Lib/WebSpider/webspider.py
@@ -6,9 +6,19 @@ url='http://www.esrl.noaa.gov/psd/data/climateindices/list/'
 request=urllib2.Request(url)
 response = urllib2.urlopen(request)
 content=response.read()
+pattern=re.findall(r'/psd/data/correlation/\S*\.data',content)
+for string in pattern:
+	NewUrl='http://www.esrl.noaa.gov/'+string
+	NewRequest=urllib2.Request(NewUrl)
+	NewResponse=urllib2.urlopen(NewRequest)
+	NewContent=NewResponse.read()
+	name=string[22:-5]
+	f=open(name,'w')
+	f.write(NewContent)
+	f.close()
+	print NewUrl
 
-pattern=re.findall(r'/psd/data/correlation/.*\.data',content)
-print pattern
 '''
 url2='http://www.esrl.noaa.gov/psd/data/correlation/'
 '''
+'/psd/data/correlation/sahelrain.data'
