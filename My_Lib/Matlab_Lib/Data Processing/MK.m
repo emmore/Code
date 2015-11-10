17db638feddfb06023a18b9b34ce9933a43a0a31
Penn <panbaoxiang@hotmail.com>
2015-11-10 09:21:02 -0800

    filter NaN

--- My_Lib/Matlab_Lib/Data Processing/MK.m
+++ My_Lib/Matlab_Lib/Data Processing/MK.m
@@ -1,11 +1,15 @@
 function r=MK(sequence)
+sequence=sequence(sequence>=0);
 l=size(sequence,1);
 s=0;
-for i=1:l-1
-	for j=i+1:l
-		s=s+sign(sequence(j)-sequence(i));
-	end
+r=0;
+if l~=0
+    for i=1:l-1
+        for j=i+1:l
+            s=s+sign(sequence(j)-sequence(i));
+        end
+    end
+    var=l*(2*l+5)*(l-1)/18.0;
+    r=s/sqrt(var);
 end
-var=l*(2*l+5)*(l-1)/18.0;
-r=s/sqrt(var);
 end
