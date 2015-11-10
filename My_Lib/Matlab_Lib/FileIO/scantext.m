50c458cc78ea29c7b615f58da923ae4d8ce5f8d1
Penn <panbaoxiang@hotmail.com>
2015-11-09 22:43:04 -0800

    scantext

---
+++ My_Lib/Matlab_Lib/FileIO/scantext.m
@@ -0,0 +1,11 @@
+function result=ReadFile(filename)
+fid=fopen(filename,'r');
+c=textscan(fid,'%d %f %f %f %f %f %f %f %f %f %f %f %f',66,'HeaderLines',1,'emptyValue', NaN);
+result=[];
+for i=1:12
+	result=[result c{i+1}];
+end
+end
+
+
+
