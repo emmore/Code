2b6a9452d5f7123dff88474c8a2c3d5abd8d86d3
Penn <panbaoxiang@hotmail.com>
2015-11-10 09:11:58 -0800

    change

--- My_Lib/Matlab_Lib/Data Processing/MK.m
+++ My_Lib/Matlab_Lib/Data Processing/MK.m
@@ -1,14 +1,11 @@
 function r=MK(sequence)
-sequence=squeeze(sequence);
-l=size(sequence,1);
+l=size(sequence);
 s=0;
 for i=1:l-1
 	for j=i+1:l
 		s=s+sign(sequence(j)-sequence(i));
 	end
 end
-r=size(l);
-%var=l*(2*l+5)*(l-1)/18.0;
- 
-%r=s/sqrt(var);
+var=l*(2*l+5)*(l-1)/18.0;
+r=s/sqrt(var);
 end
