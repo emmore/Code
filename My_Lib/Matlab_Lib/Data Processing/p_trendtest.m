2033544f156f2897edba3672561bc3c4b397bca7
Penn <panbaoxiang@hotmail.com>
2015-11-10 10:39:36 -0800

    as

--- My_Lib/Matlab_Lib/Data Processing/p_trendtest.m
+++ My_Lib/Matlab_Lib/Data Processing/p_trendtest.m
@@ -11,3 +11,39 @@ for i=1:size(t_result,1)
         ttr(i,j)=MK(t_result(i,j,47*12+1:size(t_result,3)));
     end
 end
+
+for i=1:size(ttr,1)
+    for j=1:size(ttr,2)
+        if ttr(i,j)==0
+            ttr(i,j)=NaN;
+        end
+    end
+end
+
+%{
+for i=1:size(ttr,1)
+    for j=1:size(ttr,2)
+        if isreal(ttr(i,j))
+            if ttr(i,j)>1.96
+                ttr(i,j)=1;
+            else 
+                if ttr(i,j)<-1.96
+                    ttr(i,j)=-1;
+                else
+                    if ttr(i,j)>=-1.96 && ttr(i,j)<=1.96
+                        ttr(i,j)=0;
+                    end
+                end
+            end
+        end
+    end
+end
+%} 
+
+surf(ttr');
+axis off;
+colorbar;
+view([0,90]);
+grid off;
+title('MK Statistic of Air Temperature');
+saveas(gcf,'mk_t.png');
