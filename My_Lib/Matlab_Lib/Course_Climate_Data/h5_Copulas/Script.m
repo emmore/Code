9202689f5ced889155a2a8028fb4f43d52a2f953
Penn <panbaoxiang@hotmail.com>
2016-02-11 10:37:00 -0800

    as

--- My_Lib/Matlab_Lib/Course_Climate_Data/h5_Copulas/Script.m
+++ My_Lib/Matlab_Lib/Course_Climate_Data/h5_Copulas/Script.m
@@ -2,3 +2,32 @@ cd 'C:\Users\chrs-134\Documents\Github\Code\My_Lib\Matlab_Lib\Course_Climate_Dat
 data=load('td.txt');
 precip=data(:,1);
 soilm=data(:,2);
+
+
+data=load('td.txt');
+dp=data(:,1);
+ds=data(:,2);
+disp=empdis(dp);
+diss=empdis(ds);
+
+copname='Frank';
+theta=copulafit(copname,[disp,diss]);
+value=copulacdf(copname,[disp,diss],theta);
+surf(dp,ds,value);
+
+y=norminv(value);
+
+value_e=emp_biv([dp,ds]);
+
+ddp=0.01:0.01:0.99;
+dds=0.01:0.01:0.99;
+para_cop=[ddp',dds',copulacdf(copname,[ddp',dds'],theta)];
+
+result=[];
+for i=0.01:0.05:0.99
+    dds=0.01:0.05:0.99;
+    dds=dds';
+    ddp=i*ones(length(dds),1);
+    para_cop=copulacdf(copname,[ddp,dds],theta);
+    result=[result;[ddp,dds,para_cop]];
+end
