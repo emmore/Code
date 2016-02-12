1b85566fbca462b2beabf0ec09a7ae3423448a5a
Penn <panbaoxiang@hotmail.com>
2016-02-11 16:08:14 -0800

    why isn't is working?

--- My_Lib/Matlab_Lib/Course_Climate_Data/h5_Copulas/Script.m
+++ My_Lib/Matlab_Lib/Course_Climate_Data/h5_Copulas/Script.m
@@ -1,33 +1,52 @@
 cd 'C:\Users\chrs-134\Documents\Github\Code\My_Lib\Matlab_Lib\Course_Climate_Data\h5_Copulas';
 data=load('td.txt');
-precip=data(:,1);
-soilm=data(:,2);
-
-
-data=load('td.txt');
 dp=data(:,1);
 ds=data(:,2);
 disp=empdis(dp);
 diss=empdis(ds);
 
-copname='Frank';
+%copname='Frank';
+copname='Gumbel';
 theta=copulafit(copname,[disp,diss]);
-value=copulacdf(copname,[disp,diss],theta);
-surf(dp,ds,value);
+
+
+N=20;
+u = linspace(0,1,N);
+[u1,u2] = meshgrid(u,u);
+y = copulacdf(copname,[u1(:),u2(:)],theta);
+[C,h]=contour(u1,u2,reshape(y,N,N),10);
+clabel(C,h)
+xlabel('c.d.f. of Precipitation')
+ylabel('c.d.f. of Soil Mositure')
+title(['Copulas of Precipitation and Soil Moisture Estimated with ' copname]);
+
+[dp1,ds1]=meshgrid(dp(1:5:396),ds(1:5:396));
+[disp1,diss1]=meshgrid(disp(1:5:396),diss(1:5:396));
+value=copulacdf(copname,[disp1(:),diss1(:)],theta);
+[C,h]=contour(dp1,ds1,reshape(value,length(disp1),length(diss1)),10);
+clabel(C,h)
+xlabel('c.d.f. of Precipitation')
+ylabel('c.d.f. of Soil Mositure')
+title(['Copulas of Precipitation and Soil Moisture Estimated with ' copname]);
+
+
+
+
+
+[xi,yi]=meshgrid(dp,ds);
+z=griddata(dp,ds,value,xi,yi);
+contour(xi,yi,z);
+
+[xi,yi]=meshgrid(disp,diss);
+z=griddata(disp,diss,value,xi,yi);
+contour(xi,yi,z);
+
+
 
 y=norminv(value);
 
 value_e=emp_biv([dp,ds]);
 
-ddp=0.01:0.01:0.99;
-dds=0.01:0.01:0.99;
-para_cop=[ddp',dds',copulacdf(copname,[ddp',dds'],theta)];
+ 
 
-result=[];
-for i=0.01:0.05:0.99
-    dds=0.01:0.05:0.99;
-    dds=dds';
-    ddp=i*ones(length(dds),1);
-    para_cop=copulacdf(copname,[ddp,dds],theta);
-    result=[result;[ddp,dds,para_cop]];
-end
+ 
