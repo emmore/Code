523aec881f8511403f9e479cf0b4d8f9132433cc
Penn <panbaoxiang@hotmail.com>
2016-02-16 10:51:58 -0800

    ann success

--- My_Lib/Matlab_Lib/Data Processing/script.m
+++ My_Lib/Matlab_Lib/Data Processing/script.m
@@ -1,6 +1,6 @@
 n=5;%number of PC
 m=6;%m is the input number; m*n is the input size
-
+L=20;%neuron number of middle layer.
 
 p=SCORE(:,1:n);
 for i=1:size(p,2)
@@ -8,7 +8,17 @@ for i=1:size(p,2)
     p(:,i)=(tempt-min(tempt))./(max(tempt)-min(tempt));
 end
 
-
+I=[];
+O=[];
 for i=1:length(p)-m-1
-    
+    input=reshape(p(i:i+m-1,:),[m*n,1]);
+    output=p(i+m,:)';
+    I=[I,input];
+    O=[O,output];
+end
 
+R=repmat([0 1],m*n,1);
+S=[L,n];
+net=newff(R,S,{'tansig','purelin'});
+net=train(net,I,O);
+Os=sim(net,I);
