63763bab5390d066715027d425aca49ce74107c1
Penn <panbaoxiang@hotmail.com>
2016-02-24 11:35:27 -0800

    duration_intensity

---
+++ My_Lib/Matlab_Lib/Course_Climate_Data/Final Project/Duration_Intensity.m
@@ -0,0 +1,17 @@
+function [Duration,Intensity]=Duration_Intensity(SPI)
+threshould=0;
+nSPI=find(SPI<=threshould);
+End_point=[nSPI(diff(nSPI)>1);nSPI(end)];
+Duration=diff([0;find(diff(nSPI)>1);length(nSPI)]);
+Intensity=[];
+for i=1:length(Duration)
+    Intensity=[Intensity;sum(SPI(End_point(i)-Duration(i)+1:End_point(i)))];
+end
+end
+    
+    
+
+
+
+
+    
