e95604a2cd4a46f69f867248f59be6b58b7a1a32
Penn <panbaoxiang@hotmail.com>
2016-02-25 11:18:15 -0800

    draw time pie

--- My_Lib/Matlab_Lib/Course_Climate_Data/Final Project/Duration_Intensity.m
+++ My_Lib/Matlab_Lib/Course_Climate_Data/Final Project/Duration_Intensity.m
@@ -1,17 +1,15 @@
 function [Duration,Intensity]=Duration_Intensity(SPI)
 threshould=0;
 nSPI=find(SPI<=threshould);
-End_point=[nSPI(diff(nSPI)>1);nSPI(end)];
-Duration=diff([0;find(diff(nSPI)>1);length(nSPI)]);
-Intensity=[];
-for i=1:length(Duration)
-    Intensity=[Intensity;sum(SPI(End_point(i)-Duration(i)+1:End_point(i)))];
+if isempty(nSPI)
+    Duration=NaN;
+    Intensity=NaN;
+else
+    End_point=[nSPI(diff(nSPI)>1);nSPI(end)];
+    Duration=diff([0;find(diff(nSPI)>1);length(nSPI)]);
+    Intensity=[];
+    for i=1:length(Duration)
+        Intensity=[Intensity;sum(SPI(End_point(i)-Duration(i)+1:End_point(i)))];
+    end
 end
 end
-    
-    
-
-
-
-
-    
