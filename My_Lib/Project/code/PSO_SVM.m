1e54e8f77aac95f5f7d2a63d8885810bb1409170
Penn <panbaoxiang@hotmail.com>
2016-04-19 11:16:19 -0700

    change
    try no cross validation

--- My_Lib/Project/code/PSO_SVM.m
+++ My_Lib/Project/code/PSO_SVM.m
@@ -1,4 +1,4 @@
-function [result Oe Ov]=PSO_SVM(pc)
+function [result ,Oe, Ov]=PSO_SVM(pc)
 clc;
 load('/Users/penn/Documents/Code/Github/My_Lib/Project/data/p_result.mat');
 load('/Users/penn/Documents/Code/Github/My_Lib/Project/data/full_index.mat');
@@ -31,17 +31,18 @@ for time=1:3
         end
     end
     LE=ceil(length(O)*0.7);
-%    Ie=mapminmax(I(:,1:LE));
-%    Iv=mapminmax(I(:,LE+1:length(I)));
-%    Oe=mapminmax(O(1,1:LE));
-%    Ov=mapminmax(O(1,LE+1:length(O)));
+    Ie=mapminmax(I(:,1:LE));
+    Iv=mapminmax(I(:,LE+1:length(I)));
+    Oe=mapminmax(O(1,1:LE));
+    Ov=mapminmax(O(1,LE+1:length(O)));
+%{
     I=mapminmax(I);
     O=mapminmax(O);
     Ie=I(:,1:LE);
     Iv=I(:,LE+1:length(I));
     Oe=mapminmax(O(1,1:LE));
     Ov=mapminmax(O(1,LE+1:length(O)));
-    
+%}
     
     Ie=Ie';
     Iv=Iv';
@@ -62,14 +63,16 @@ for time=1:3
             community{j}.p=[100 1].*rand(1,2);
             community{j}.v=0.2*[100,1].*rand(1,2);
             community{j}.best=community{j}.p;
-			a=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -v 5 -p 0.2 -q -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
-            b=1;
+			a=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -v 2 -p 0.2 -q 0 -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
+            
+            model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -p 0.2 -q 0 -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
+            b=sqrt(mean((svmpredict(double(Ov),double(Iv),model)-Ov).^2));
             %b=svmtrain(double(Ov),double(Iv),['-s 3 -t 2 -v 3 -p 0.1 -q -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
             %a=sqrt(mean((svmpredict(double(Oe),double(Ie),model)-Oe).^2));
             %b=sqrt(mean((svmpredict(double(Ov),double(Iv),model)-Ov).^2));
             community{j}.performance=[a,b];
             community{j}.bperformance=[a,b];
-            if community{j}.performance(1,1)<n_performance(1,1)%&&community{j}.performance(1,2)>n_performance(1,2)
+            if community{j}.performance(1,1)<n_performance(1,1)%&&community{j}.performance(1,2)<n_performance(1,2)
                n_performance=community{j}.performance;
                g_best=community{j}.p;
             end
@@ -79,11 +82,13 @@ for time=1:3
          for j=1:N
                 community{j}.v=community{j}.v+c1*rand*(community{j}.best-community{j}.p)+c2*rand*(g_best-community{j}.p);
                 community{j}.p=community{j}.p+community{j}.v;
-				community{j}.p(1)=max(community{j}.p(1),20);
+				community{j}.p(1)=max(community{j}.p(1),15);
 				community{j}.p(2)=max(community{j}.p(2),0.01);
-				a=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -v 5 -p 0.2 -q -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
-                b=1;
-                %b=svmtrain(double(Ov),double(Iv),['-s 3 -t 2 -v 3 -p 0.1 -q -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
+				a=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -v 2 -p 0.2 -q 0 -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
+                
+                model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -p 0.2 -q  0 -c ' num2str(community{j}.p(1)) ' -g '  num2str(community{j}.p(2))]);
+                b=sqrt(mean((svmpredict(double(Ov),double(Iv),model)-Ov).^2));
+                 
                 community{j}.performance=[a,b];
                 disp(community{j}.performance);
                 if community{j}.performance(1,1)<community{j}.bperformance(1)%&&community{j}.performance(1,2)>community{j}.bperformance(2)
@@ -92,18 +97,19 @@ for time=1:3
                     if community{j}.performance(1,1)<n_performance(1,1)%&&community{j}.performance(1,2)>n_performance(1,2)
                         n_performance=community{j}.performance;
                         g_best=community{j}.p;
-                        if n_performance(1,1)<result{time}.performance(1,1)%&&n_performance(1,2)>result{time}.performance(1,2)
-                            result{time}.performance=n_performance;
-                            result{time}.p=g_best;
-                        end
+
                     end
                 end
         end  
     end
+    if n_performance(1,1)<result{time}.performance(1,1)%&&n_performance(1,2)>result{time}.performance(1,2)
+         result{time}.performance=n_performance;
+         result{time}.p=g_best;
+    end
     x=result{time}.select;
     disp(x);
     disp(n_performance);
-    model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -p 0.2 -q -c ' num2str(g_best(1)) ' -g '  num2str(g_best(2))]);
+    model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2 -p 0.2 -q 0 -c ' num2str(g_best(1)) ' -g '  num2str(g_best(2))]);
     result{time}.Ose=svmpredict(double(Oe),double(Ie),model);
     %model=svmtrain(double(Ov),double(Iv),['-s 3 -t 2 -p 0.1 -q -c ' num2str(result{time}.p(1)) ' -g '  num2str(result{time}.p(2))]);
     result{time}.Osv=svmpredict(double(Ov),double(Iv),model);
