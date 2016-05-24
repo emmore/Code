83a96225aa097a6eb7dd64f9853bbaf9a2a3ed2c
Penn <panbaoxiang@hotmail.com>
2016-05-24 09:47:55 -0700

    as

--- My_Lib/Project/code/PSO_SVM.m
+++ My_Lib/Project/code/PSO_SVM.m
@@ -1,21 +1,21 @@
-function [result ,Oe, Ov]=PSO_SVM(pc)
+function [result_e,result_v,ssr_le,ssr_lv,ssr_svme,ssr_svmv]=PSO_SVM()
 clc;
 load('/Users/penn/Documents/Code/Github/My_Lib/Project/data/p_result.mat');
 load('/Users/penn/Documents/Code/Github/My_Lib/Project/data/climate_index.mat');
  
 %%%%%%%%%Implement PCA to precipiation data%%%%%%%%%%%%%%
-SCORE=LPCA_p(p_result,0);
-m=1;%m is the input delaying length
-%%%%%%%%%Select the pc(th) eof.
-p=SCORE(:,pc);
-%%%%%%%%%Iteratively select inputs
+[COEFF,SCORE,latent]=LPCA_p(p_result,0);
+m=2;%m is the input delaying length
 
-    rng('shuffle');
+mpc=4;
+for pc=1:mpc
+%%%%%%%%%Select the pc(th) eof.
+    p=SCORE(:,pc);
+%%%%%%%%%Iteratively select inputs
 %%%%%%%%%Input Selection%%%%%%%%%%%%%
-    result{time}.select=rand(1,size(index,2))>0.65*ones(1,size(index,2));
-    result{time}.performance=[100 100];
-    for i=1:size(result{time}.select,2)
-        if result{time}.select(i)==1;
+    indexselect=[0,1,1,0,1,1,0,0,0,1,0];
+    for i=1:length(indexselect)
+        if indexselect(i)==1;
             p=[p index(:,i)];
         end
     end
@@ -34,6 +34,8 @@ p=SCORE(:,pc);
     LE=ceil(length(O)*0.85);
     Ie=mapminmax(I(:,1:LE));
     Iv=mapminmax(I(:,LE+1:length(I)));
+    Adj_e(pc,:)=[min(O(1,1:LE)),max(O(1,1:LE))];
+    Adj_v(pc,:)=[min(O(1,LE+1:length(O))),max(O(1,LE+1:length(O)))];
     Oe=mapminmax(O(1,1:LE));
     Ov=mapminmax(O(1,LE+1:length(O)));
     Ie=Ie';
@@ -41,35 +43,30 @@ p=SCORE(:,pc);
     Oe=Oe';
     Ov=Ov';
     
-    %%%%%%%%%%%%%linear%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%Linear Model%%%%%%%%%%%%%%%%%
     lIe=[Ie, ones(size(Ie,1),1)];
     lIv=[Iv, ones(size(Iv,1),1)];
     fac=(lIe'*lIe)\lIe'*Oe;
     lsOe=lIe*fac;
     lsOv=lIv*fac;
-    ssr_le=sqrt(mean((lsOe-Oe).^2));
-    ssr_lv=sqrt(mean((lsOv-Ov).^2));
+    ssr_le(pc)=sqrt(mean((lsOe-Oe).^2));
+    ssr_lv(pc)=sqrt(mean((lsOv-Ov).^2));
     
-    
-    
-    
-    
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% particle swarm optimizer for initial weights and b
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%Parameters for PSO%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%SVM Model%%%%%%%%%%%%%%%%%%%%
     N=200;%number of particles in one community
     c1=1.0;%rate of following community best
     c2=1.0;%rate of following historical best
     T=4;%iteration times
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     clear community;
     n_performance=[100,100];
     for j=1:N
-            community{j}.p=[300 1].*rand(1,2);
-            community{j}.v=0.2*[150,1].*rand(1,2);
+            community{j}.p(1)=2^(-10+j*15.0/N);
+            community{j}.p(2)=2^(-14+j*12.0/N);
+            community{j}.v(1)=0.05*community{j}.p(1);
+            community{j}.v(2)=0.05*community{j}.p(2);
             community{j}.best=community{j}.p; 
-            model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(community{j}.p(2))  ' -c ' num2str(community{j}.p(1)) ' -p 0.25 -q ']);
+            model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(community{j}.p(2))  ' -c ' num2str(community{j}.p(1)) ' -p 0.18 -q ']);
             a=sqrt(mean((svmpredict(double(Oe),double(Ie),model,'-q')-Oe).^2));
             b=sqrt(mean((svmpredict(double(Ov),double(Iv),model,'-q')-Ov).^2));
             community{j}.performance=[a,b];
@@ -87,9 +84,9 @@ p=SCORE(:,pc);
 				community{j}.p(1)=max(community{j}.p(1),15);
 				community{j}.p(2)=max(community{j}.p(2),0.01); 
                 
-                a=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(community{j}.p(2))  ' -c ' num2str(community{j}.p(1)) ' -p 0.25 -q -v 5']);
+                a=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(community{j}.p(2))  ' -c ' num2str(community{j}.p(1)) ' -p 0.18 -q -v 5']);
                 %a=sqrt(mean((svmpredict(double(Oe),double(Ie),model,'-q')-Oe).^2));
-                model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(community{j}.p(2))  ' -c ' num2str(community{j}.p(1)) ' -p 0.25 -q ']);
+                model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(community{j}.p(2))  ' -c ' num2str(community{j}.p(1)) ' -p 0.18 -q ']);
                 b=sqrt(mean((svmpredict(double(Ov),double(Iv),model,'-q')-Ov).^2));
                  
                 community{j}.performance=[a,b];
@@ -104,17 +101,23 @@ p=SCORE(:,pc);
                 end
         end  
      end
-    if n_performance(1,1)<result{time}.performance(1,1)%&&n_performance(1,2)>result{time}.performance(1,2)
-         result{time}.performance=n_performance;
-         result{time}.performance(1)=sqrt(mean((svmpredict(double(Oe),double(Ie),model,'-q')-Oe).^2));
-         result{time}.p=g_best;
-    end
-    x=result{time}.select;
-    disp(x);
-    disp(n_performance);
-    model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(g_best(2))  ' -c ' num2str(g_best(1)) ' -p 0.25 -q ']);
-    result{time}.Ose=svmpredict(double(Oe),double(Ie),model,'-q');
-    %model=svmtrain(double(Ov),double(Iv),['-s 3 -t 2 -p 0.1 -q -c ' num2str(result{time}.p(1)) ' -g '  num2str(result{time}.p(2))]);
-    result{time}.Osv=svmpredict(double(Ov),double(Iv),model,'-q');
- 
+    model=svmtrain(double(Oe),double(Ie),['-s 3 -t 2  -g '  num2str(g_best(2))  ' -c ' num2str(g_best(1)) ' -p 0.18 -q ']);
+    
+    svmOe(:,pc)=svmpredict(double(Oe),double(Ie),model,'-q');
+    svmOv(:,pc)=svmpredict(double(Ov),double(Iv),model,'-q');
+    ssr_svme(pc)=sqrt(mean((svmOe(:,pc)-Oe).^2));
+    ssr_svmv(pc)=sqrt(mean((svmOv(:,pc)-Ov).^2));
 end
+OE_SVM=[];
+OV_SVM=[];
+for pc=1:mpc
+    tem1=Adj_e(pc,:);
+    OE_SVM=[OE_SVM,svmOe(:,pc)*(tem1(2)-tem1(1))+tem1(1)];
+    
+    tem2=Adj_v(pc,:);
+    OV_SVM=[OV_SVM,svmOv(:,pc)*(tem2(2)-tem2(1))+tem2(1)];
+end
+result_e=OE_SVM*COEFF(1:mpc,:);
+result_v=OV_SVM*COEFF(1:mpc,:);
+end
+
